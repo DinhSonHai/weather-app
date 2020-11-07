f6fa94067c55b404e2c3831b9110326f07c7ce49
DinhSonHai <sdinhhai@gmail.com>
2020-11-07 23:05:59 +0700

    Add background photo that change with temperature

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 const config = require('config');
 const api = {
   key: config.get('WEATHER_API_KEY'),
+  base: 'https://api.openweathermap.org/data/2.5/'
 }
 function App() {
   return (
-    <div className="App">
-
+    <div className="app">
+      <main>
+        <div className="search-box">
+          <input type="text" className="search-bar" placeholder="Search..."/> 
+        </div>
+      </main>
     </div>
   );
 }
