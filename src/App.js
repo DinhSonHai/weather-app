77cb638cb0052293a5787f2127c1ef40bf4e0ca3
DinhSonHai <sdinhhai@gmail.com>
2020-11-11 23:25:08 +0700

    Add search function to get weather data from user input

--- src/App.js
+++ src/App.js
@@ -3,6 +3,19 @@ const api = {
   base: 'https://api.openweathermap.org/data/2.5/'
 }
 function App() {
+
+  const search = evt => {
+    if (evt.key === "Enter") {
+      fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+        .then(res => res.json())
+        .then(result => {
+          setWeather(result);
+          setQuery('');
+          console.log(result);
+        });
+    }
+  }
+
   const dateBuilder = (d) => {
     let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
     let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
