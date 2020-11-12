b5a759793e348cd5d7267883ab93237e7a9a2ec2
DinhSonHai <sdinhhai@gmail.com>
2020-11-12 23:15:11 +0700

    Add state to set query string to get weather data from user input

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+import { useState } from 'react';
 const api = {
   key: process.env.REACT_APP_WEATHER_API_KEY,
   base: 'https://api.openweathermap.org/data/2.5/'
 }
 function App() {
+
+  const [query, setQuery] = useState('');
+  const [weather, setWeather] = useState({});
 
   const search = evt => {
     if (evt.key === "Enter") {
@@ -28,15 +32,32 @@ function App() {
     return `${day} ${date} ${month} ${year}`;
   }
   return (
-    <div className="app">
+    <div className={(typeof weather.main != "undefined") ? ((weather.main.temp > 16) ? 'app warm' : 'app') : 'app'}>
       <main>
         <div className="search-box">
-          <input type="text" className="search-bar" placeholder="Search..."/> 
+          <input 
+            type="text" 
+            className="search-bar" 
+            placeholder="Search..." 
+            onChange={e => setQuery(e.target.value)}
+            value={query}
+            onKeyPress={search}
+          /> 
         </div>
-        <div className="location-box">
-          <div className="location">New York City, US</div>
-          <div className="date">{dateBuilder(new Date())}</div>
+        {(typeof weather.main != "undefined") ? (
+        <div>
+          <div className="location-box">
+            <div className="location">{weather.name}, {weather.sys.country}</div>
+            <div className="date">{dateBuilder(new Date())}</div>
+          </div>
+          <div className="weather-box">
+            <div className="temp">
+              {Math.round(weather.main.temp)}°c
+            </div>
+            <div className="weather">{weather.weather[0].main}</div>
+          </div>
         </div>
+        ) : ('')}
       </main>
     </div>
   );
